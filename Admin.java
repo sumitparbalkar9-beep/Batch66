10460e84ecf9c5c7ff88030c1d626e564d88912d
sumitparbalkar9-beep <sumitparbalkar9@gmail.com>
2026-01-19 10:28:12 +0530

    Add 2 files in java

---
+++ Admin.java
@@ -0,0 +1,11 @@
+package com.test;
+import com.c.R;
+public class Admin{
+
+public static void main(String[] args){
+System.out.println("Main Method");
+System.out.println(R.k);
+R.m3();
+
+}
+}
---
+++ classTest.java
@@ -0,0 +1,14 @@
+package com.test;
+import com.c.R;
+public class Admin{
+
+public static void main(String[] args){
+System.out.Println("Main Method");
+System.out.Println(R.k);
+R.m3();
+
+
+
+
+}
+}
